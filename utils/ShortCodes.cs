53f18433a306c3a3d40250878edfb65fc1d9e348
Vincent Nwonah <vnwonah@outlook.com>
2019-06-28 16:40:01 +0100

    added shortcodes

---
+++ utils/ShortCodes.cs
@@ -0,0 +1,29 @@
+﻿namespace utils
+{
+    public static class ShortCodes
+    {
+        //Main Menu ShortCodes
+        public const string SELF_SERVICE_CODE = "1";
+        public const string TRANSACTIONS_CODE = "2";
+        public const string EXIT_CODE = "3";
+
+        //Self Service Short Codes
+        public const string OPEN_RSA_CODE = "1-1";
+        public const string GENERATE_RSA_PIN_CODE = "1-2";
+        public const string UNIT_PRICE_CODE = "1-3";
+        public const string BRANCH_LOCATION_CODE = "1-3";
+        public const string GENERATE_PIN_CODE = "1-4";
+        public const string SIGMA_INFORMATION_CODE = "1-6";
+
+        //Transactions Code
+        public const string RSA_BALANCE_CODE = "2-1";
+        public const string LAST_FUNDING_PERIOD_CODE = "2-2";
+        public const string LAST_AMOUNT_CONTRIBUTED_CODE = "2-3";
+        public const string LAST_TRANSACTION_DATE_CODE = "2-4";
+        public const string RETIREMENT_BENFIT_APPLICATION_STATUS_CODE = "2-5";
+        public const string STATEMENT_OF_ACCOUNT_CODE = "2-6";
+
+        //Global Back
+        public const string BACK_CODE = "7";
+    }
+}
